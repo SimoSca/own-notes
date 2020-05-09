85e6667e96236063329d61cc1edefaa4fc05aafc
simonescardoni <s.scardoni@logotel.it>
2020-05-09 20:36:32 +0200

    Updates

---
+++ precache-manifest.3cb680365cd63def31af4e00b66efca5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92c35b22b3203ae2e3ca7ad5a957010",
+    "url": "/own-notes/index.html"
+  },
+  {
+    "revision": "55b64e0057c3f158c034",
+    "url": "/own-notes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "acf6352efd19a44b1c6c",
+    "url": "/own-notes/static/js/2.5d1cff28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/own-notes/static/js/2.5d1cff28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55b64e0057c3f158c034",
+    "url": "/own-notes/static/js/main.43d4790a.chunk.js"
+  },
+  {
+    "revision": "2f5e552a29725a73ee95",
+    "url": "/own-notes/static/js/runtime-main.ebdf1654.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/own-notes/static/media/logo.5d5d9eef.svg"
+  }
+]);

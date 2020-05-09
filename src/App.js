b137c80fdb683b3515d65001279e7ffc2daba568
simonescardoni <s.scardoni@logotel.it>
2020-05-09 22:20:51 +0200

    [new][git-hook] added the right git hook

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          # Learn React #
+          # Start with test... #
         </a>
       </header>
     </div>
